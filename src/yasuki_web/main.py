6c60cd34333015917b1b884402266de5e233ac38
jessegrabowski <jessegrabowski@gmail.com>
2026-03-21 17:40:25 -0400

    Restrict CORS methods and headers

--- src/yasuki_web/main.py
+++ src/yasuki_web/main.py
@@ -29,8 +29,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=_cors_origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization"],
 )
 
 DECK_BUILDER_DIR = Path(__file__).parent / "static" / "deck_builder"
